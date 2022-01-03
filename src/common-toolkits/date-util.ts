ab9bdfa8e74289a70ea90b8f7144764ce4f9ced4
Matrix <maoshiba2006@126.com>
2022-01-03 23:01:57 +0800

    update

--- src/common-toolkits/date-util.ts
+++ src/common-toolkits/date-util.ts
@@ -6,24 +6,15 @@ import { DateType, DateTypeInterface, FormatDate, Period } from './types';
  * @param format
  * @returns
  */
-const currFormat = (format = FormatDate.SECONDS_FORMAT) => {
-  return moment().format(format);
-};
+const currFormat = (format = FormatDate.SECONDS_FORMAT) => moment().format(format);
 
 /**
  * 获取时间戳
  * @returns
  */
-const timeStamp = (): string => {
-  return currFormat(FormatDate.SECONDS_FORMAT);
-};
+const timeStamp = (): string => currFormat(FormatDate.SECONDS_FORMAT);
 
-const toMoment = (
-  value: DateTypeInterface,
-  format = FormatDate.DAY_FORMAT,
-): Moment => {
-  return moment.isMoment(value) ? value : moment(value, format);
-};
+const toMoment = ( value: DateTypeInterface, format = FormatDate.DAY_FORMAT ): Moment => moment.isMoment(value) ? value : moment(value, format);
 
 /**
  * 时间范围 当天，当周，当月，当季，当年 等
@@ -31,22 +22,11 @@ const toMoment = (
  * @param rest
  * @returns
  */
-const range = <T extends Moment | string>(
-  _period: Period,
-  rest: { dateType?: DateType; format?: string; rang?: number[] } = {},
-): T[] => {
-  const {
-    dateType = DateType.dateString,
-    rang = [],
-    format = FormatDate.SECONDS_FORMAT,
-  } = rest;
+const range = <T extends Moment | string>(_period: Period, rest: { dateType?: DateType; format?: string; rang?: number[] } = {} ): T[] => {
+  const { dateType = DateType.dateString, rang = [], format = FormatDate.SECONDS_FORMAT } = rest;
   const hasRange = rang.length === 2;
-  const beginDate = hasRange
-    ? moment().subtract(rang[0], Period[_period] as DurationInputArg2)
-    : moment().startOf(Period[_period] as unitOfTime.StartOf);
-  const endDate = hasRange
-    ? moment().subtract(rang[1], Period[_period] as DurationInputArg2)
-    : moment().endOf(Period[_period] as unitOfTime.StartOf);
+  const beginDate = hasRange ? moment().subtract(rang[0], Period[_period] as DurationInputArg2) : moment().startOf(Period[_period] as unitOfTime.StartOf);
+  const endDate = hasRange ? moment().subtract(rang[1], Period[_period] as DurationInputArg2) : moment().endOf(Period[_period] as unitOfTime.StartOf);
   if (dateType === DateType.dateMoment) {
     return [beginDate, endDate] as T[];
   }
@@ -61,12 +41,7 @@ const range = <T extends Moment | string>(
  * @param format
  * @returns
  */
-const diff = (
-  beginDate: DateTypeInterface,
-  endDate: DateTypeInterface,
-  _period: Period,
-  format = FormatDate.DAY_FORMAT,
-): number => {
+const diff = ( beginDate: DateTypeInterface, endDate: DateTypeInterface, _period: Period, format = FormatDate.DAY_FORMAT ): number => {
   const _beginDate = toMoment(beginDate, format);
   const _endDate = toMoment(endDate, format);
   return _endDate.diff(_beginDate, Period[_period] as unitOfTime.Diff);
@@ -79,11 +54,7 @@ const diff = (
  * @param format
  * @returns
  */
-const comparison = (
-  beginDate: DateTypeInterface,
-  endDate: DateTypeInterface,
-  format = FormatDate.DAY_FORMAT,
-): boolean => {
+const comparison = ( beginDate: DateTypeInterface, endDate: DateTypeInterface, format = FormatDate.DAY_FORMAT ): boolean => {
   const _beginDate = toMoment(beginDate, format);
   const _endDate = toMoment(endDate, format);
   return _endDate.diff(_beginDate) > 0;
