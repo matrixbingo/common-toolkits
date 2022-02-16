62cb318005abfef3d9bf274b704009f83dc3e7ee
Liang.wang <maoshiba2006@126.com>
2022-02-16 18:13:58 +0800

    Update transform-util.ts

--- src/common-toolkits/transform-util.ts
+++ src/common-toolkits/transform-util.ts
@@ -159,7 +159,42 @@ const toArrByPaths = (arr: any[], paths: string[]): any => {
     return rs;
   }, {} as any);
 };
+/**
+ * 把 K V 结构的对象转换成数组
+ * @param obj {1: 'a1', 2: 'b2}
+ * @param customizer {key: 'id', value: 'name' }
+ * @returns [{id: 1, name: 'a1'}, {id: 2, name: 'b2'}]
+ */
+const objectToArray = (obj: ObjectType, customizer: {key: string; value: string} | ((item: [string, any]) => ObjectType) = { key: 'id', value: 'name' }, keyType: 'int' | 'string' = 'string'): ObjectType[] => {
+  if (isEmpty(obj)) return [] as ObjectType[];
+  return Object.entries(obj).reduce((rs, next) => {
+    if (isFunction(customizer)) {
+      rs.push(customizer(next));
+    } else {
+      rs.push({ [customizer.key]: keyType === 'int' ? lodash.parseInt(next[0]) : next[0], [customizer.value]: next[1] });
+    }
+    return rs;
+  }, [] as ObjectType[]);
+};
+
+/**
+ * proComponents, proTable 常用
+ * @param obj 
+ * @param customizer 
+ * @returns 
+ */
+const objecToValueEnum = (obj: ObjectType, customizer: {key: string; value: string} | ((item: [string, any]) => any) = { key: 'id', value: 'text' }): any => {
+  return Object.entries(obj).reduce((rs, next) => {
+    if (isFunction(customizer)) {
+      rs[next[0]] = customizer(next);
+    } else {
+      rs[next[0]] = { text: next[1] };
+    }
+    return rs;
+  }, {});
+};
 
 export default {
-  mapKeys, nullToString, numberArrToStringArr, objectMapKeys, options, select, stringArrToMumberArr, toArrByPath, toArrByPathUnique, toArrByPaths,
+  mapKeys, nullToString, numberArrToStringArr, objectMapKeys, objectToArray, objecToValueEnum, options, select, stringArrToMumberArr, toArrByPath, toArrByPathUnique, toArrByPaths,
 } as const;
+
